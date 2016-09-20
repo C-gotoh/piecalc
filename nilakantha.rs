dd72cb9351a8c1aa537ea5a2746580f984085660
C-gotoh <shigotoh1@gmail.com>
2016-09-20 22:10:15 +0200

    calculating pie with the nilakantha series

---
+++ nilakantha.rs
@@ -0,0 +1,20 @@
+use std::env;
+
+fn main() {
+    let mut p = 3.0;
+    let mut count = 2.0;
+    let mut sign = true;
+    let args: Vec<String> = env::args().collect();
+    let it = args[1].parse::<i32>().unwrap();
+    println!("Making Pie");
+    for x in 0..it {
+        if sign {
+            p += 4.0/(count*(count+1.0)*(count+2.0)); }
+        else {
+            p -= 4.0/(count*(count+1.0)*(count+2.0));
+        }
+        sign = !sign;
+        count += 2.0;
+    }
+    println!("Pie is {}", p);
+}
